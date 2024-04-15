8f7f11b0ca6e497edcc0cc414e2c80e1a3bf62f0
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-04-15 21:39:01 +0100

    Working with Hooks to avoid recoding

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,13 +1,22 @@
 import { test } from "@playwright/test";
 
-test.describe("test suite 1",  () => {//Here we create a suit of tests that can have more than one test
-  test("the first test of this suite", async ({page}) => {
-    await page.goto('http://localhost:4200/');
-    await page.getByText('Forms').click();
-    await page.getByText('Form Layouts').click();
+//it means we will execute this before all suites we have
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+});
+
+//Here we create a suit of tests that can have more than one test
+test.describe("test suite 1", () => {
+  //this one will execute before each test inside this suite
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
   });
 
-  test("the second test of this suite", () => {
-    
+  test("the first test of this suite", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("the second test of this suite", async ({ page }) => {
+    await page.getByText("Datepicker").click();
   });
 });
