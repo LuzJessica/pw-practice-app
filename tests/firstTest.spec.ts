23872ed25b001f2bbda18fbf224ef53ace7e28b2
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-04-18 18:43:23 +0100

    Learning some locator syntax rules

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,22 +1,42 @@
 import { test } from "@playwright/test";
+import { first } from "rxjs-compat/operator/first";
 
 //it means we will execute this before all suites we have
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
 });
 
-//Here we create a suit of tests that can have more than one test
-test.describe("test suite 1", () => {
-  //this one will execute before each test inside this suite
-  test.beforeEach(async ({ page }) => {
-    await page.getByText("Forms").click();
-  });
+test('Locator syntax rules', async({page}) => {
+  //By Tag Name
+  await page.locator('input').first().click();//if we use the tag name, it's important to inform which element with this tag name we want because we can find many
 
-  test("the first test of this suite", async ({ page }) => {
-    await page.getByText("Form Layouts").click();
-  });
+  //By ID
+  await page.locator('#inputEmail1').click();
 
-  test("the second test of this suite", async ({ page }) => {
-    await page.getByText("Datepicker").click();
-  });
-});
+  //By Class value
+  await page.locator('.shape-rectangle').first().click();
+
+  //By Attribute
+  page.locator('[placeholder="Email"]')
+
+  //By full Class value
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+  //Combining different selector
+  page.locator('input[placeholder="Email"][nbinput]')//DO NOT add space between selectors in this case!!
+
+  //By XPath (NOT RECOMMENDED)
+  page.locator('//*[@id="inputEmail1"]')
+
+  //By Partial text match
+  page.locator(':text("Using")')
+
+  //By exact text Match
+  page.locator(':text-is("Using the Grid")')
+})
+
+
+
+
