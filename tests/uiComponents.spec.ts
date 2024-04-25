f709bff9eea5ea3739c1c11d403e2895e5c1257f
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-04-25 21:58:52 +0100

    Learning how to validate browser dialog boxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -103,6 +103,20 @@ test.describe("Form Layouts page", () => {
     const toolTip = await page.locator('nb-tooltip').textContent();
     expect(toolTip).toEqual('This is a tooltip');
 
+  })
+
+  test('dialog boxes', async ({page}) => {
+    await page.getByTitle('Tables & Data').click();
+    await page.getByTitle('Smart Table').click();
+    
+
+    page.on('dialog', dialog => {
+        expect(dialog.message()).toEqual('Are you sure you want to delete?');
+        dialog.accept();
+    })
+
+    await page.getByRole('table').locator('tr', {hasText: "mdo@gmail.com"}).locator('.nb-trash').click();
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com');
 
   })
 
