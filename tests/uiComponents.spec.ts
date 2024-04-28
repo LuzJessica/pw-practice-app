571d79f8d3d93875641ee412a641be40f3b5a889
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-04-28 16:09:57 +0100

    Working with sliders and movement of mouse

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -228,3 +228,39 @@ test.describe("Form Layouts page", () => {
     await expect(calendarInputField).toHaveValue(dateToAssert);
   });
 });
+
+test('sliders', async ({page}) => {
+  /*update slider attribute
+  const tempGauge = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle');
+  await tempGauge.evaluate( node => {
+    node.setAttribute('cx', '232.630');
+    node.setAttribute('cy', '232.630');
+  })
+  await tempGauge.click();*/
+
+  //mouse movement
+const tempBox = page.locator('[tabtitle="Temperature"] ngx-temperature-dragger');
+await tempBox.scrollIntoViewIfNeeded();
+
+const box = await tempBox.boundingBox();
+const x = box.x + box.width / 2;
+const y = box.y + box.height / 2;
+await page.mouse.move(x,y);
+await page.mouse.down();
+await page.mouse.move(x+100, y);
+await page.mouse.move(x+100, y+100);
+await page.mouse.up();
+await expect(tempBox).toContainText('30');
+
+
+
+
+
+
+
+
+
+
+
+})
+
