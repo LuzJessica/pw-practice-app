1639b06a5c824686889ff04286c8e0304e14326e
Jéssica Luz <qae.jessicaluz@gmail.com>
2024-06-05 16:36:56 +0100

    Using js faker plugin to create data to test

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
+import { faker } from '@faker-js/faker'
 
 
 
@@ -20,10 +21,13 @@ test('navigate to form page', async({page}) => {
 
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page);
+    const randomName = faker.name.firstName('female');
+    const randomEmail = `${randomName.replace(' ','')}${faker.random.numeric(4)}@test.com`;
+
     await pm.navigateTo().formLayoutsPage();
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndOption('tst@test.com', 'Welcome1', 'Option 1');
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('Jéssica Luz', 'jessicaluz@test.com', true);
-    await pm.navigateTo().datePickerPage();  
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomName, randomEmail, true);
+   /* await pm.navigateTo().datePickerPage();  
     await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(14);
-    await pm.onDatepickerPage().selectDatepickerWithRange(6,15);
+    await pm.onDatepickerPage().selectDatepickerWithRange(6,15);*/
 })
